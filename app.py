75043b25261bd335bea18693e7b57fad46670741
wanitamulyaasri <118180537+wanitamulyaasri@users.noreply.github.com>
2022-11-25 23:32:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ outflow_fig.add_scatter(name='Outflow', x=df_outflow['Bulan'], y=df_outflow['Dat
 outflow_fig.layout.title = 'Outflow'
 
 simulation_fig = go.FigureWidget()
-# simulation_fig.add_scatter(name='Outflow', x=df_outflow['Bulan'], y=df_outflow['Data'])
+simulation_fig.add_scatter(name='Outflow', x=df_outflow['Bulan'], y=df_outflow['Data'])
 simulation_fig.layout.title = 'Simulation'
 
 
